b8dd3124070bb27d2d1b6aa8e61649b17144c7e5
BRENOLIVEIRApng <brenoreisrv@gmail.com>
2025-10-30 10:40:23 -0300

    criacao do spring

---
+++ GT/src/test/java/brenoliveiral/png/gerennciadorDeTarefas/GT/GtApplicationTests.java
@@ -0,0 +1,13 @@
+package brenoliveiral.png.gerennciadorDeTarefas.GT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
