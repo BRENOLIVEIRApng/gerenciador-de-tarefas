ad5441b984561c20189b95f22164e9a78caee0f7
BRENOLIVEIRApng <brenoreisrv@gmail.com>
2025-10-30 12:25:11 -0300

    configurando o thymeleaf

---
+++ GT/src/main/java/brenoliveiral/png/gerenciadorDeTarefas/GT/config/TymeleafConfig.java
@@ -0,0 +1,28 @@
+package brenoliveiral.png.gerenciadorDeTarefas.GT.config;
+
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.templateresource.SpringResourceTemplateResource;
+import org.thymeleaf.templatemode.TemplateMode;
+
+public class TymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setPrefix("classpth:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+}
